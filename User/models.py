629fdc332cbe655408fa2591e17c9295b46b3d3a
Birajit <birajit95@gmail.com>
2020-12-22 21:59:02 +0530

    [Birajit] Add: user registration model is added

--- User/models.py
+++ User/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(30)
+    lastName = models.CharField(30)
+    userName = models.CharField(30)
+    email = models.CharField(40)
+    password = models.CharField(100)
+    date = models.DateTimeField(default=datetime.utcnow())
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
