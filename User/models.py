65d6c8c5479af16b3b6387816e951eb0cd9e371b
Birajit <birajit95@gmail.com>
2020-12-22 22:05:24 +0530

    [Birajit] Refactor: user model is refactored

--- User/models.py
+++ User/models.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 
 class User(models.Model):
-    firstName = models.CharField(30)
-    lastName = models.CharField(30)
-    userName = models.CharField(30)
-    email = models.CharField(40)
-    password = models.CharField(100)
-    date = models.DateTimeField(default=datetime.utcnow())
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    userName = models.CharField(max_length=30)
+    email = models.CharField(max_length=40)
+    password = models.CharField(max_length=100)
+    date = models.DateTimeField(default=datetime.utcnow)
 
     def __str__(self):
         return f"{self.firstName} {self.lastName}"
