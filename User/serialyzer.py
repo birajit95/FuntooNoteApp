08d0de78d5262748ee864b578a86b50dc71fd79c
Birajit <birajit95@gmail.com>
2020-12-25 00:05:57 +0530

    [Birajit] Refactor: User registration feature is refactored

---
+++ User/serialyzer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'is_active']
+
+    def validate_password(self, password):
+        return make_password(password)
+
--- User/views.py
+++ User/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from rest_framework.views import APIView
-from django.views import View
-from .models import User
-from django.contrib.auth.hashers import make_password
+from .serialyzer import UserSerializer
+from rest_framework.renderers import JSONRenderer
+from django.contrib.auth.models import User
 
 
 class UserRegistration(APIView):
-
     def get(self, request):
         return render(request, "userRegistrration.html")
 
     def post(self, request):
-        firstName = request.POST['firstName']
-        lastName = request.POST['lastName']
-        userName = request.POST['userName']
-        email = request.POST['email']
-        password = request.POST['pass1']
-        hashPass = make_password(password)
-        User(firstName=firstName, lastName=lastName, userName=userName, email=email, password=hashPass).save()
-        return HttpResponse("Registration is done")
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.get(username=serializer.data.get('username'))
+            user.is_active = False
+            user.save()
+            response = {'msg': "data is created"}
+            return HttpResponse(JSONRenderer().render(response))
+        return HttpResponse(JSONRenderer().render(serializer.errors))
