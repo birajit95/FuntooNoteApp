247810f04f4f35fbb666c48ec64fac35f199509f
Birajit <birajit95@gmail.com>
2020-12-25 00:03:57 +0530

    [Birajit] Add: Profile Model is registered

--- User/admin.py
+++ User/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import Profile
 
-admin.site.register(User)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id','user','bio','dob']
+
+
