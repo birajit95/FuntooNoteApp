51eb049ad11713a0ed4e8b89712d0a1dad54bbfc
Birajit <birajit95@gmail.com>
2020-12-25 22:05:51 +0530

    [Birajit] Add: user login url is configured

--- User/urls.py
+++ User/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import UserRegistration, VerifyEmail
+from .views import UserRegistration, VerifyEmail, UserLogin
 
 urlpatterns = [
     path('registration/', UserRegistration.as_view(), name="userRegistration"),
     path('verify-email/', VerifyEmail.as_view(), name="verifyEmail"),
-
+    path('login/', UserLogin.as_view(), name="userLogin"),
 ]
