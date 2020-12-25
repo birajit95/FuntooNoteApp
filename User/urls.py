ba2d9538f72b626d0b1b162f633d2d4ccbd03d1f
Birajit <birajit95@gmail.com>
2020-12-25 17:20:22 +0530

    [Birajit] Add: email verification url is set

--- User/urls.py
+++ User/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistration
+from .views import UserRegistration, VerifyEmail
 
 urlpatterns = [
     path('registration/', UserRegistration.as_view(), name="userRegistration"),
+    path('verify-email/', VerifyEmail.as_view(), name="verifyEmail"),
 
 ]
