e7af5dcd3fe397fec3c43edfed1b974d18c36960
Birajit <birajit95@gmail.com>
2020-12-25 23:42:28 +0530

    [Birajit] Add: Login API is developed

--- User/views.py
+++ User/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from .JWTAuthentication import JWTAuth
 from .emailVarification import Email
+from django.contrib.auth import authenticate, login
 
 
 class UserRegistration(APIView):
@@ -28,7 +29,6 @@ class UserRegistration(APIView):
             return HttpResponse(JSONRenderer().render(response))
         return HttpResponse(JSONRenderer().render(serializer.errors))
 
-
 class VerifyEmail(APIView):
     def get(self, request):
         jwtToken = request.GET.get('token')
@@ -43,3 +43,25 @@ class VerifyEmail(APIView):
         responseMsg = {'msg': 'Your account is activated! Now you can log in'}
         return HttpResponse(JSONRenderer().render(responseMsg))
 
+
+class UserLogin(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            responseMsg = {'msg': 'You are logged in successfully'}
+            return HttpResponse(JSONRenderer().render(responseMsg))
+        user = User.objects.get(username=username)
+        if user:
+            jwtToken = JWTAuth.getToken(user.username, user.password)
+            current_site = get_current_site(request).domain
+            relative_url = 'user/verify-email/'
+            email_data = Email.configureEmail(jwtToken, user, current_site, relative_url)
+            Email.sendEmail(email_data)
+            responseMsg = {'msg': 'Your account is not active. Please activate your account from the link shared in '
+                                  'your mail '}
+            return HttpResponse(JSONRenderer().render(responseMsg))
+        responseMsg = {'msg': 'Bad Credential found'}
+        return HttpResponse(JSONRenderer().render(responseMsg))
