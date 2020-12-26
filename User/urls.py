e357076cee23cf5b2b47d642968181817ca13b64
Birajit <birajit95@gmail.com>
2020-12-26 10:17:24 +0530

    [Birajit] Add: user logout url is configured

--- User/urls.py
+++ User/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegistration, VerifyEmail, UserLogin
+from .views import UserRegistration, VerifyEmail, UserLogin, logoutUser
 
 urlpatterns = [
     path('registration/', UserRegistration.as_view(), name="userRegistration"),
     path('verify-email/', VerifyEmail.as_view(), name="verifyEmail"),
     path('login/', UserLogin.as_view(), name="userLogin"),
+    path('logout/', logoutUser, name="userLogout"),
 ]
