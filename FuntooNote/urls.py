2fc6b1cec9d68b9a257130542f93689a63ce8c20
Birajit <birajit95@gmail.com>
2020-12-26 09:34:31 +0530

    [Birajit] fix: redundent codes are cleaned

--- FuntooNote/urls.py
+++ FuntooNote/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
-# schema_view = get_swagger_view(title="API Documentation")
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="SWAGGER API",
+      title="FUNTOONOTES API DOCUMENTATION",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -34,16 +32,11 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-
-]
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("User.urls")),
-    # path('api_doc/', schema_view)
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
