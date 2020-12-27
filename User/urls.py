5251a0221fa34dc55580be776ee6722e4a9ab961
Birajit <birajit95@gmail.com>
2020-12-27 11:38:07 +0530

    [Birajit] Add: forgot password and reset password urls are configured

--- User/urls.py
+++ User/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import UserRegistration, VerifyEmail, UserLogin, logoutUser
+from .views import UserRegistration, VerifyEmail, UserLogin, logoutUser, ForgotPassword, ResetPassword
 
 urlpatterns = [
     path('registration/', UserRegistration.as_view(), name="userRegistration"),
     path('verify-email/', VerifyEmail.as_view(), name="verifyEmail"),
     path('login/', UserLogin.as_view(), name="userLogin"),
     path('logout/', logoutUser, name="userLogout"),
+    path('forgot-password/', ForgotPassword.as_view(), name="forgotPassword"),
+    path('reset-password/<uid>/<token>/', ResetPassword.as_view(), name="resetPassword"),
+
 ]
