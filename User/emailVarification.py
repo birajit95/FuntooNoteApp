fa7ac2a19431c7c107b286365fc2105da9f5e5e0
Birajit <birajit95@gmail.com>
2020-12-27 11:45:06 +0530

    [Birajit] Add: Password rest email is configured

--- User/emailVarification.py
+++ User/emailVarification.py
@@ -10,6 +10,13 @@ class Email:
         return email_data
 
     @staticmethod
+    def configureResetPasswordMail(token,user, current_site, relative_url):
+        absoluteURL = "http://" + current_site + '/' + relative_url + "?token=" + str(token)
+        email_body = f"Hi {user.username}! use the link bellow to reset your password \n {absoluteURL}"
+        email_data = {'email_body': email_body, 'email_subject': 'Reset Password', 'to_email': user.email}
+        return email_data
+
+    @staticmethod
     def sendEmail(data):
         email = EmailMessage(
             subject=data['email_subject'],
