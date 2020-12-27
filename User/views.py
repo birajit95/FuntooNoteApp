bf9713fdf4abd2236c94b13883a68f32c06afcc5
Birajit <birajit95@gmail.com>
2020-12-27 12:22:19 +0530

    [Birajit] Fix: Bug fixed for swagger API

--- User/views.py
+++ User/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from rest_framework.views import APIView
-from .serialyzer import UserSerializer, ResetPasswordSerializer, ForgotPasswordSerializer
+from .serialyzer import UserSerializer, ResetPasswordSerializer, ForgotPasswordSerializer, UserLoginSerializer
 from rest_framework.renderers import JSONRenderer
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
@@ -11,36 +11,16 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.decorators import login_required
+from rest_framework.generics import GenericAPIView
 
 
-class UserRegistration(APIView):
+class UserRegistration(GenericAPIView):
+    serializer_class =  UserSerializer
     def get(self, request):
         return render(request, "userRegistrration.html")
 
-    first_name = openapi.Parameter(
-        'first_name', in_= openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-    last_name = openapi.Parameter(
-        'last_name', in_=openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-    username = openapi.Parameter(
-        'username', in_= openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-    email = openapi.Parameter(
-        'email', in_=openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-    password = openapi.Parameter(
-        'password', in_=openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-
-    @swagger_auto_schema(manual_parameters=[first_name, last_name, username, email, password])
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
             user = User.objects.get(username=serializer.data.get('username'))
@@ -57,6 +37,11 @@ class UserRegistration(APIView):
 
 
 class VerifyEmail(APIView):
+    token = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description',
+        type=openapi.TYPE_STRING
+    )
+    @swagger_auto_schema(manual_parameters=[token])
     def get(self, request):
         jwtToken = request.GET.get('token')
         response = JWTAuth.verifyToken(jwtToken=jwtToken)
@@ -71,40 +56,34 @@ class VerifyEmail(APIView):
         return HttpResponse(JSONRenderer().render(responseMsg))
 
 
-class UserLogin(APIView):
-    username = openapi.Parameter(
-        'username', in_=openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-    password = openapi.Parameter(
-        'password', in_=openapi.IN_QUERY, description='Description',
-        type=openapi.TYPE_STRING
-    )
-
-    @swagger_auto_schema(manual_parameters=[username, password])
+class UserLogin(GenericAPIView):
+    serializer_class = UserLoginSerializer
     def post(self, request):
-        username = request.data['username']
-        password = request.data['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            responseMsg = {'msg': 'You are logged in successfully'}
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data.get('username')
+            password = serializer.data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                responseMsg = {'msg': 'You are logged in successfully'}
+                return HttpResponse(JSONRenderer().render(responseMsg))
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                user = None
+            if user and check_password(password, user.password):
+                jwtToken = JWTAuth.getToken(user.username, user.password)
+                current_site = get_current_site(request).domain
+                relative_url = 'user/verify-email/'
+                email_data = Email.configureEmail(jwtToken, user, current_site, relative_url)
+                Email.sendEmail(email_data)
+                responseMsg = {'msg': 'Your account is not active. Please activate your account from the link shared in '
+                                      'your mail '}
+                return HttpResponse(JSONRenderer().render(responseMsg))
+            responseMsg = {'msg': 'Bad Credential found'}
             return HttpResponse(JSONRenderer().render(responseMsg))
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            user = None
-        if user and check_password(password, user.password):
-            jwtToken = JWTAuth.getToken(user.username, user.password)
-            current_site = get_current_site(request).domain
-            relative_url = 'user/verify-email/'
-            email_data = Email.configureEmail(jwtToken, user, current_site, relative_url)
-            Email.sendEmail(email_data)
-            responseMsg = {'msg': 'Your account is not active. Please activate your account from the link shared in '
-                                  'your mail '}
-            return HttpResponse(JSONRenderer().render(responseMsg))
-        responseMsg = {'msg': 'Bad Credential found'}
-        return HttpResponse(JSONRenderer().render(responseMsg))
+        return HttpResponse(JSONRenderer().render(serializer.errors))
 
 @login_required(login_url='/user/login/')
 def logoutUser(request):
@@ -112,7 +91,7 @@ def logoutUser(request):
     responseMsg = {'msg': 'You are logged out successfully'}
     return HttpResponse(JSONRenderer().render(responseMsg))
 
-class ForgotPassword(APIView):
+class ForgotPassword(GenericAPIView):
     serializer_class = ForgotPasswordSerializer
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -131,7 +110,8 @@ class ForgotPassword(APIView):
             return HttpResponse(JSONRenderer().render(responseMsg))
         return HttpResponse(JSONRenderer().render(serializer.errors))
 
-class ResetPassword(APIView):
+class ResetPassword(GenericAPIView):
+    serializer_class = ResetPasswordSerializer
     def get(self, request, uid, token):
         response = JWTAuth.verifyToken(jwtToken=token)
         if not response:
@@ -144,7 +124,7 @@ class ResetPassword(APIView):
         return HttpResponse(JSONRenderer().render(responseMsg))
 
     def put(self, request, uid, token):
-        serializer = ResetPasswordSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             password = serializer.data.get('password')
             response = JWTAuth.verifyToken(jwtToken=token)
