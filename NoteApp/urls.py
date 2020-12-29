3eac8afa6dab0aab14d55b61ef09a972608ca7f9
Birajit <birajit95@gmail.com>
2020-12-29 21:24:16 +0530

    [Birajit] Add: notes deletion url is configured

--- NoteApp/urls.py
+++ NoteApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllNotesAPI, AddNotesAPI, UpdateNotesAPI
+from .views import AllNotesAPI, AddNotesAPI, UpdateNotesAPI, DeleteNotesAPI
 urlpatterns = [
     path('', AllNotesAPI.as_view(), name="allNotes"),
     path('add-note/', AddNotesAPI.as_view(), name='addNote'),
-    path('update-note/<int:note_id>', UpdateNotesAPI.as_view(), name='updateNote')
+    path('update-note/<int:note_id>/', UpdateNotesAPI.as_view(), name='updateNote'),
+    path('delete-note/<int:note_id>/', DeleteNotesAPI.as_view(), name='deleteNote')
     ]
