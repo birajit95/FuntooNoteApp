bbc39c7168bc0aae496d667a031681e8a142dfe8
Birajit <birajit95@gmail.com>
2020-12-29 07:53:47 +0530

    [Birajit] NoteApp database model is designed and registered

--- NoteApp/models.py
+++ NoteApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    date = models.DateTimeField(default = datetime.now())
+    label = models.CharField(max_length=20, blank=True, null=True)
+    is_archive = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
