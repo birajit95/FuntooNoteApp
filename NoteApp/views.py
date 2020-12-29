16619fee9cc9e05d106f5a450d7ea81a0625765f
Birajit <birajit95@gmail.com>
2020-12-29 10:08:23 +0530

    [Birajit] Add: notes adding API is developed

--- NoteApp/views.py
+++ NoteApp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework.generics import GenericAPIView
-from .serializer import RetriveAllNotesSerializer
+from .serializer import RetriveAllNotesSerializer, AddNotesAPISerializer
 from .models import Notes
 from django.db.models import Q
+from rest_framework import status
 
 class AllNotesAPI(GenericAPIView):
     serializer_class = RetriveAllNotesSerializer
@@ -11,3 +12,14 @@ class AllNotesAPI(GenericAPIView):
         allNotes = Notes.objects.filter(Q(is_archive=False) & Q(user=request.user.pk))
         serializer = self.serializer_class(allNotes, many=True)
         return HttpResponse(JSONRenderer().render(serializer.data))
+
+class AddNotesAPI(GenericAPIView):
+    serializer_class = AddNotesAPISerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            Notes(user=request.user, title=serializer.data.get('title'),
+                  content=serializer.data.get('content')).save()
+            responseMsg = {'msg':'Your note is saved', 'status':status.HTTP_201_CREATED}
+            return HttpResponse(JSONRenderer().render(responseMsg))
+        return HttpResponse(JSONRenderer().render(serializer.errors))
