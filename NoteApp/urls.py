047c586dc0e8ae394e76867dc5f5b06943375b5c
Birajit <birajit95@gmail.com>
2020-12-29 12:13:39 +0530

    [Birajit] Add: note updating url is configured

--- NoteApp/urls.py
+++ NoteApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AllNotesAPI, AddNotesAPI
+from .views import AllNotesAPI, AddNotesAPI, UpdateNotesAPI
 urlpatterns = [
     path('', AllNotesAPI.as_view(), name="allNotes"),
-    path('add-note/', AddNotesAPI.as_view(), name='addNote')
+    path('add-note/', AddNotesAPI.as_view(), name='addNote'),
+    path('update-note/<int:note_id>', UpdateNotesAPI.as_view(), name='updateNote')
     ]
