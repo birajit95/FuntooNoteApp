620557565bbab1365b25ee349cb37a4d3e9d04fe
Birajit <birajit95@gmail.com>
2020-12-29 09:03:14 +0530

    [Birajit] Add: notes listing API is developed

--- NoteApp/views.py
+++ NoteApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.generics import GenericAPIView
+from .serializer import RetriveAllNotesSerializer
+from .models import Notes
+from django.db.models import Q
 
-# Create your views here.
+class AllNotesAPI(GenericAPIView):
+    serializer_class = RetriveAllNotesSerializer
+    def get(self, request):
+        allNotes = Notes.objects.filter(Q(is_archive=False) & Q(user=request.user.pk))
+        serializer = self.serializer_class(allNotes, many=True)
+        return HttpResponse(JSONRenderer().render(serializer.data))
