2a05a6bca30f4f54913da267d1b2e6a840e59467
Birajit <birajit95@gmail.com>
2020-12-29 09:13:14 +0530

    [Birajit] Add: Notes adding serializer is added and validated

--- NoteApp/serializer.py
+++ NoteApp/serializer.py
@@ -6,3 +6,13 @@ class RetriveAllNotesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
         fields = ['title', 'content', 'date', 'label']
+
+
+class NotesAPISerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['title', 'content']
+    def validate(self, data):
+        if len(data.get('title')) < 2 and len(data.get('content')) < 2 :
+            raise serializers.ValidationError('Too Short Notes Title or Content')
+        return data
