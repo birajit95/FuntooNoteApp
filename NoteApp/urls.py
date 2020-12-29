f8c120f498b4cbcbdb9c96fa1494b201e329fc90
Birajit <birajit95@gmail.com>
2020-12-29 10:23:43 +0530

    [Birajit] Fix: a bug is fixed

--- NoteApp/urls.py
+++ NoteApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AllNotesAPI, AddNotesAPI
 urlpatterns = [
     path('', AllNotesAPI.as_view(), name="allNotes"),
-    path('add-note/', AllNotesAPI.as_view(), name='addNote')
+    path('add-note/', AddNotesAPI.as_view(), name='addNote')
     ]
