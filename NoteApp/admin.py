18e2d614f047bf64eac99cd6a33daf7f175f1a8e
Birajit <birajit95@gmail.com>
2020-12-30 00:37:16 +0530

    [Birajit] Refactor: Notes model is refactored

--- NoteApp/admin.py
+++ NoteApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Notes
+from .models import Notes, Label
 @admin.register(Notes)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user','title','content','date','label', 'is_archive']
+
+admin.site.register(Label)
--- NoteApp/models.py
+++ NoteApp/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
 
+
+class Label(models.Model):
+    label_name = models.CharField(max_length=20, primary_key=True)
+
+    def __str__(self):
+        return self.label_name
+
 class Notes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=500)
     date = models.DateTimeField(default = datetime.now())
-    label = models.CharField(max_length=20, blank=True, null=True)
+    label = models.ForeignKey(Label, blank=True, null=True, on_delete=models.CASCADE)
     is_archive = models.BooleanField(default=False)
 
     class Meta:
