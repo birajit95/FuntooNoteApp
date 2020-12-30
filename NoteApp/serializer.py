04bb7b8372a1daee8ed36575dab5dc18da6bc603
Birajit <birajit95@gmail.com>
2020-12-30 21:01:57 +0530

    [Birajit] Refactor: Note add/update serializer is refactored

--- NoteApp/serializer.py
+++ NoteApp/serializer.py
@@ -11,7 +11,7 @@ class RetriveAllNotesSerializer(serializers.ModelSerializer):
 class AddOrUpdateNotesAPISerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
-        fields = ['title', 'content', 'label']
+        fields = ['title', 'content']
     def validate(self, data):
         if len(data.get('title')) < 2 and len(data.get('content')) < 2 :
             raise serializers.ValidationError('Too Short Notes Title or Content')
