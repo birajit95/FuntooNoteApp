a1f903002e27f4620a97e4cea89a8272e477ee28
Birajit <birajit95@gmail.com>
2021-01-02 00:04:00 +0530

    [Birajit] Refactor: Serializer is refactored

--- NoteApp/serializer.py
+++ NoteApp/serializer.py
@@ -3,16 +3,32 @@ from rest_framework import serializers
 
 
 class RetriveAllNotesSerializer(serializers.ModelSerializer):
+    label = serializers.CharField(max_length=30)
     class Meta:
         model = Notes
         fields = ['title', 'content', 'date', 'label','last_updated']
 
+    def validate(self, data):
+        try:
+            data['label'] = Label.objects.get(label_name=data.get('label'))
+        except Label.DoesNotExist:
+            data['label'] = None
+        return data
+
+
 class AddOrUpdateNotesAPISerializer(serializers.ModelSerializer):
+    label = serializers.CharField(max_length=30)
     class Meta:
         model = Notes
         fields = ['title', 'content', 'label']
+
     def validate(self, data):
-        if len(data.get('title')) < 2 and len(data.get('content')) < 2 :
+        try:
+            label = Label.objects.get(label_name=data.get('label'))
+            data['label'] = label
+        except Label.DoesNotExist:
+            data['label'] = None
+        if len(data.get('title')) < 2 or len(data.get('content')) < 2 :
             raise serializers.ValidationError('Too Short Notes Title or Content')
         return data
 
