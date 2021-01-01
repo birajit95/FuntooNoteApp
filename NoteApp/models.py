37bcc9b6dfd7f9ba3f1ccdcad8636a967d88278d
Birajit <birajit95@gmail.com>
2021-01-01 21:52:47 +0530

    [Birajit] Refactor: Notes and label models are refactored

--- NoteApp/models.py
+++ NoteApp/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class Label(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    label_name = models.CharField(max_length=20, primary_key=True)
+    label_name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.label_name
@@ -15,8 +15,10 @@ class Notes(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=500)
     date = models.DateTimeField(default = datetime.now())
-    label = models.ForeignKey(Label, blank=True, null=True, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, blank=True, null=True, on_delete=models.DO_NOTHING)
     is_archive = models.BooleanField(default=False)
+    is_trash = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         ordering = ['-date']
