6b1fdeff79fa0b107dc4bca5ad032c7bcd2be93f
Birajit <birajit95@gmail.com>
2021-01-03 10:56:18 +0530

    [Birajit] Add: Image field is added in profile model

--- User/models.py
+++ User/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from datetime import datetime
-
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     dob = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='profile_pics',default='default.jpg')
+
+    def save(self):
+        super(Profile, self).save()
+        img = Image.open(self.image.path)
+        if img.height > 300 and img.width > 300:
+            img.thumbnail((300,300))
+            img.save(self.image.path)
+
 
 
 @receiver(post_save, sender=User)
