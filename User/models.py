8a3222232f2d11b499438cbefa742973a5773955
Birajit <birajit95@gmail.com>
2021-01-04 21:40:05 +0530

    [Birajit] Refactor: User Profile model is refactored

--- User/models.py
+++ User/models.py
@@ -9,10 +9,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     dob = models.DateField(null=True, blank=True)
-    image = models.ImageField(upload_to='profile_pics',default='default.jpg')
+    image = models.ImageField(upload_to='profile_pics',default='profile_pics/default.jpg')
 
-    def save(self):
-        super(Profile, self).save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 and img.width > 300:
             img.thumbnail((300,300))
