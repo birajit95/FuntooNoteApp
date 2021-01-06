276cfb3fb64a057bd0e3f461ea2fdc05aa5e5692
Birajit <birajit95@gmail.com>
2021-01-07 01:01:33 +0530

    [Birajit] Refactor: Notes App serializers are refactored

--- NoteApp/serializer.py
+++ NoteApp/serializer.py
@@ -2,37 +2,28 @@ from .models import Notes, Label
 from rest_framework import serializers
 
 
+class LabelAPISerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ['label_name']
+
+
 class RetriveAllNotesSerializer(serializers.ModelSerializer):
-    label = serializers.CharField(max_length=30)
+    label = LabelAPISerializer(many=True)
     class Meta:
         model = Notes
         fields = ['title', 'content', 'date', 'label','last_updated']
 
-    def validate(self, data):
-        try:
-            data['label'] = Label.objects.get(label_name=data.get('label'))
-        except Label.DoesNotExist:
-            data['label'] = None
-        return data
-
+class LabelSerializer(serializers.Serializer):
+    label_name = serializers.CharField(max_length=30)
 
 class AddOrUpdateNotesAPISerializer(serializers.ModelSerializer):
-    label = serializers.CharField(max_length=30)
+    label = LabelSerializer(many=True)
     class Meta:
         model = Notes
         fields = ['title', 'content', 'label']
 
     def validate(self, data):
-        try:
-            label = Label.objects.get(label_name=data.get('label'))
-            data['label'] = label
-        except Label.DoesNotExist:
-            data['label'] = None
         if len(data.get('title')) < 2 or len(data.get('content')) < 2 :
             raise serializers.ValidationError('Too Short Notes Title or Content')
         return data
-
-class LabelAPISerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Label
-        fields = ['label_name']
