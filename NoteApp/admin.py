71709ff3b49b132cf891dbbd8178fdcc9189f39a
Birajit <birajit95@gmail.com>
2021-01-07 12:35:00 +0530

    [Birajit] Refactor: Notes Admin is refactored

--- NoteApp/admin.py
+++ NoteApp/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from .models import Notes, Label
-@admin.register(Notes)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user','title','content','date','label', 'is_archive']
 
-@admin.register(Label)
-class LabelAdmin(admin.ModelAdmin):
-    list_display = ['user', 'label_name']
+admin.site.register(Notes)
+admin.site.register(Label)
