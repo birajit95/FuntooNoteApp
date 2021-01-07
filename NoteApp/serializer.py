6cbf6beaf86638b1fc0d9b5fcb2c3f971a0288db
Birajit <birajit95@gmail.com>
2021-01-07 10:18:57 +0530

    [Birajit] Refactor: Note and label serializer are refactored

--- NoteApp/serializer.py
+++ NoteApp/serializer.py
@@ -1,5 +1,6 @@
 from .models import Notes, Label
 from rest_framework import serializers
+from django.db.models import Q
 
 
 class LabelAPISerializer(serializers.ModelSerializer):
@@ -15,22 +16,34 @@ class RetriveAllNotesSerializer(serializers.ModelSerializer):
         fields = ['title', 'content', 'date', 'label','last_updated']
 
 class LabelSerializer(serializers.Serializer):
-    label_name = serializers.CharField(max_length=30)
+    label_name = serializers.CharField(max_length=30, allow_blank=True, allow_null=True)
 
-    def validate(self, data):
-        try:
-            Label.objects.get(label_name=data.get('label_name'))
-        except:
-            raise serializers.ValidationError(f"'{data.get('label_name')}' label is not Found")
-        return data
 
 class AddOrUpdateNotesAPISerializer(serializers.ModelSerializer):
-    label = LabelSerializer(many=True)
+    label = LabelSerializer(many=True, required=False, default=None)
     class Meta:
         model = Notes
         fields = ['title', 'content', 'label']
 
     def validate(self, data):
+        if not data.get('label'):
+            data['label'] = None
         if len(data.get('title')) < 2 or len(data.get('content')) < 2 :
             raise serializers.ValidationError('Too Short Notes Title or Content')
         return data
+
+    def create(self, validated_data):
+        label_data = validated_data.pop('label')
+        validated_data['user'] = self.context['user']
+        note = Notes.objects.create(**validated_data)
+        if label_data:
+            for label in label_data:
+                if not label['label_name']:
+                    break
+                try:
+                    lb_obj = Label.objects.get(Q(label_name=label['label_name']) & Q(user=note.user))
+                    note.label.add(lb_obj)
+                except Label.DoesNotExist:
+                    note.delete()
+                    raise serializers.ValidationError({'response_msg': f"{label['label_name']} label is not exist"})
+        return note
