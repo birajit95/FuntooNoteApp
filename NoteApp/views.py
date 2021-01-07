36dca1e0c9477d209b190f9c657395a73d3a9389
Birajit <birajit95@gmail.com>
2021-01-07 10:43:24 +0530

    [Birajit] Refactor: Add Notes and Update Notes APIs are refactored

--- NoteApp/views.py
+++ NoteApp/views.py
@@ -23,13 +23,9 @@ class AllNotesAPI(GenericAPIView):
 class AddNotesAPI(GenericAPIView):
     serializer_class = AddOrUpdateNotesAPISerializer
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data, context={'user':request.user})
         if serializer.is_valid():
-            note = Notes(user=request.user, title=serializer.data.get('title'), content=serializer.data.get('content'))
-            note.save()
-            for label in serializer.data.get('label'):
-                    label_obj = Label.objects.get(label_name=label['label_name'])
-                    note.label.add(label_obj)
+            serializer.save()
             return Response({'response_msg':'Your note is saved'}, status=status.HTTP_201_CREATED)
         return Response({'response_msg':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -37,29 +33,28 @@ class AddNotesAPI(GenericAPIView):
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class UpdateNotesAPI(GenericAPIView):
     serializer_class = AddOrUpdateNotesAPISerializer
-    def get(self, request, note_id):
-        try:
-            note = Notes.objects.get(Q(pk=note_id) & Q(user=request.user))
-        except Notes.DoesNotExist:
-            responseMsg = {'msg': 'Your not authorised to access this data', 'status': status.HTTP_401_UNAUTHORIZED}
-            return HttpResponse(JSONRenderer().render(responseMsg))
-        serializer = self.serializer_class(note)
-        return HttpResponse(JSONRenderer().render(serializer.data))
-
     def patch(self, request, note_id):
         try:
             note = Notes.objects.get(Q(pk=note_id) & Q(user=request.user))
         except Notes.DoesNotExist:
-            responseMsg = {'msg': 'Your not authorised to access this data', 'status': status.HTTP_401_UNAUTHORIZED}
-            return HttpResponse(JSONRenderer().render(responseMsg))
-        serializer = self.serializer_class(note, data=request.data, partial=True)
+            return Response({'msg': 'Your not authorised to access this data'},status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.serializer_class(data=request.data, partial=True)
         if serializer.is_valid():
             note.last_updated = datetime.now()
+            note.title = serializer.data.get('title')
+            note.content = serializer.data.get('content')
+            for label in serializer.data.get('label'):
+                try:
+                    Label.objects.get(Q(label_name=label['label_name']) & Q(user=request.user))
+                except Label.DoesNotExist:
+                    return Response({'response_msg': 'This label is not exist'}, status=status.HTTP_404_NOT_FOUND)
+            note.label.clear()
+            for label in serializer.data.get('label'):
+                    label_obj = Label.objects.get(Q(label_name=label['label_name']) & Q(user=request.user))
+                    note.label.add(label_obj)
             note.save()
-            serializer.save()
-            responseMsg = {'msg': 'Your note is Updated', 'status': status.HTTP_201_CREATED}
-            return HttpResponse(JSONRenderer().render(responseMsg))
-        return HttpResponse(JSONRenderer().render(serializer.errors))
+            return Response({'response_msg': 'Your note is Updated'}, status=status.HTTP_200_OK)
+        return Response({'response_msg':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
