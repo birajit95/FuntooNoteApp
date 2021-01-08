b519aa16bf753018e2f3f819070ab4b733e5316f
Birajit <birajit95@gmail.com>
2021-01-08 12:27:12 +0530

    [Birajit] Add: Test cases for update note are added and tested succesfully

--- NoteApp/notes_tests/test_views.py
+++ NoteApp/notes_tests/test_views.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from ..models import Label
-from django.contrib.auth import login
+from ..models import Label, Notes
 from django.urls import reverse
 from rest_framework import status
 import json
@@ -29,6 +28,13 @@ class TestNotesAPIs(TestCase):
     def userWiseLabelData(self, user_1_label=None, user_2_label=None):
         Label.objects.create(user=self.user_1, label_name=user_1_label)
         Label.objects.create(user=self.user_2, label_name=user_2_label)
+
+    def addNote(self, title=None, content=None, label_name=None, user=None):
+        label = Label.objects.create(user=user, label_name=label_name)
+        note = Notes.objects.create(user=user, title=title,content=content)
+        note.label.add(label)
+        return note.id
+
 
     def test_add_label_when_user_is_logged_in(self, label_name='Google'):
         self.user_login(user='birajit95')
@@ -158,3 +164,101 @@ class TestNotesAPIs(TestCase):
         response = self.client.post(reverse('addNote'),data=json.dumps(valid_payload_1), content_type='application/json')
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(f"{response.data['response_msg']}",'Cat label is not exist')
+
+    # test cases for note update
+
+    def test_update_note_with_label_and_color_when_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.user_login(user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello world',
+            'content': 'Python',
+            'label': [{
+                'label_name': 'Google'
+            }],
+            'color': '#FF5733'
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                    content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_update_note_with_label_and_without_color_when_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.userWiseLabelData(user_1_label="Dog", user_2_label='Cat')
+        self.user_login(user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+            'label': [{
+                'label_name': 'Dog'
+            }]
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_update_note_without_label_and_with_color_when_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.user_login(user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+            'color': '#FF5733'
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_update_note_if_label_is_not_found_and_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.userWiseLabelData(user_1_label="Dog", user_2_label='Cat')
+        self.user_login(user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+            'label': [{
+                'label_name': 'Cat'
+            }]
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_note_if_note_id_is_not_found_and_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.userWiseLabelData(user_1_label="Dog", user_2_label='Cat')
+        self.user_login(user=self.user_1)
+        invalid_note_id=100
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+            'label': [{
+                'label_name': 'Dog'
+            }]
+        }
+        response = self.client.patch(reverse('updateNote', args=[invalid_note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_note_without_label_and_color_when_user_is_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        self.user_login(user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_update_note_when_user_is_not_logged_in(self):
+        note_id = self.addNote(title="Hello", content="World", label_name="Google", user=self.user_1)
+        valid_payload_1 = {
+            'title': 'Hello Brother',
+            'content': 'Django',
+        }
+        response = self.client.patch(reverse('updateNote', args=[note_id]), data=json.dumps(valid_payload_1),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_302_FOUND)
+        self.assertEquals(response.url, f'/user/login/?next=/notes/update-note/{note_id}/')
+
