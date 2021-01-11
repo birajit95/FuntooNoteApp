bd9a77abc15aa2d688d1fb03edd3183d9e3a5149
Birajit <birajit95@gmail.com>
2021-01-11 09:48:10 +0530

    [Birajit] Add: logger and docstring added for all the APIs of NoteApp

--- NoteApp/views.py
+++ NoteApp/views.py
@@ -7,15 +7,24 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from rest_framework.response import Response
+import sys
+sys.path.append("..")
+from FuntooNote.logger import logger
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class AllNotesAPI(GenericAPIView):
     serializer_class = RetriveAllNotesSerializer
     def get(self, request):
+        """
+        This API is used to fetch all notes of the user
+        @return: returns all notes
+        """
         allNotes = Notes.objects.filter(Q(is_archive=False) & Q(user=request.user.pk) & Q(is_trash=False))
         if not allNotes:
+            logger.info(f"{request.user.username}'s no notes present")
             return Response({'response_data': 'No notes available'}, status=status.HTTP_404_NOT_FOUND)
         serializer = self.serializer_class(allNotes, many=True)
+        logger.info(f"{request.user.username}'s all notes accessed")
         return Response({'response_data':serializer.data}, status=status.HTTP_200_OK)
 
 
@@ -23,10 +32,17 @@ class AllNotesAPI(GenericAPIView):
 class AddNotesAPI(GenericAPIView):
     serializer_class = AddOrUpdateNotesAPISerializer
     def post(self, request):
+        """
+        This API is used to create a note for user
+        @param request: It takes note title, content, label(optional) and color(optional)
+        @return: creates not on successful validation
+        """
         serializer = self.serializer_class(data=request.data, context={'user':request.user})
         if serializer.is_valid():
             serializer.save()
+            logger.info(f"{request.user.username}'s note is saved")
             return Response({'response_msg':'Your note is saved'}, status=status.HTTP_201_CREATED)
+        logger.error(serializer.errors)
         return Response({'response_msg':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -34,9 +50,16 @@ class AddNotesAPI(GenericAPIView):
 class UpdateNotesAPI(GenericAPIView):
     serializer_class = AddOrUpdateNotesAPISerializer
     def patch(self, request, note_id):
+        """
+        This API is used to update the existing note
+        @param request: title, content, label, color
+        @param note_id: primary_key of the specific note
+        @return: updates the note
+        """
         try:
             note = Notes.objects.get(Q(pk=note_id) & Q(user=request.user))
         except Notes.DoesNotExist:
+            logger.warning("Note does not exist on update API hit")
             return Response({'msg': 'Your not authorised to access this data'},status=status.HTTP_401_UNAUTHORIZED)
         serializer = self.serializer_class(data=request.data, partial=True)
         if serializer.is_valid():
@@ -49,6 +72,7 @@ class UpdateNotesAPI(GenericAPIView):
                     try:
                         Label.objects.get(Q(label_name=label['label_name']) & Q(user=request.user))
                     except Label.DoesNotExist:
+                        logger.warning(f"{label['label_name']} does not exist")
                         return Response({'response_msg': 'This label is not exist'}, status=status.HTTP_404_NOT_FOUND)
                 note.label.clear()
             if serializer.data.get('label'):
@@ -56,23 +80,33 @@ class UpdateNotesAPI(GenericAPIView):
                         label_obj = Label.objects.get(Q(label_name=label['label_name']) & Q(user=request.user))
                         note.label.add(label_obj)
             note.save()
+            logger.info("Note is updated")
             return Response({'response_msg': 'Your note is Updated'}, status=status.HTTP_200_OK)
+        logger.error(serializer.errors)
         return Response({'response_msg':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class DeleteNotesAPI(GenericAPIView):
     def delete(self, request, note_id):
+        """
+        This API is used to delete and trash existing note
+        @param note_id: primary_key of the specific note
+        @return: trash or delete the note if it is already trashed
+        """
         try:
             note = Notes.objects.get(Q(pk=note_id) & Q(user=request.user))
             if note.is_trash:
                 note.delete()
+                logger.info("Note is deleted permanently")
                 return Response({'response_msg': 'Your Note is deleted permanently'}, status=status.HTTP_200_OK)
             else:
                 note.is_trash = True
                 note.save()
+                logger.info("Note is Trashed")
                 return Response({'response_msg': 'Your Note is trashed'}, status=status.HTTP_200_OK)
         except Notes.DoesNotExist:
+            logger.warning("Note does not exist")
             return Response({'response_msg': 'This Note is not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
@@ -80,10 +114,17 @@ class DeleteNotesAPI(GenericAPIView):
 class AddLabelAPI(GenericAPIView):
     serializer_class = LabelAPISerializer
     def post(self, request):
+        """
+        This API is used to create a label
+        @param request: label name
+        @return: Creates label
+        """
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             Label(user=request.user, label_name=serializer.data.get('label_name')).save()
+            logger.info("label created")
             return Response({'response_msg':'Label added'},status=status.HTTP_201_CREATED)
+        logger.error(serializer.errors)
         return Response({'response_msg':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -91,20 +132,34 @@ class AddLabelAPI(GenericAPIView):
 class RetriveLableAPI(GenericAPIView):
     serializer_class = LabelAPISerializer
     def get(self, request):
+        """
+        This API is used to get all existing label created by the user
+        @param request: Get request
+        @return: returns all existing label
+        """
         label_data = Label.objects.filter(Q(user=request.user))
         serializer = self.serializer_class(label_data, many=True)
         if not serializer.data:
+            logger.info("No label found")
             return Response({'response_data': 'No labels found'}, status=status.HTTP_404_NOT_FOUND)
+        logger.info("All labels are accessed")
         return Response({'response_data':serializer.data}, status=status.HTTP_200_OK)
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class DeleteLabelAPI(GenericAPIView):
     def delete(self, request, label_name):
+        """
+        This API is used to delete a specific label
+        @param label_name: specific label name to be deleted
+        @return: deletes label
+        """
         try:
             label = Label.objects.get(Q(label_name=label_name) & Q(user_id=request.user.pk))
             label.delete()
+            logger.info(f"{label_name} label is deleted")
             return Response({'response_msg': 'Label deleted'}, status = status.HTTP_200_OK)
         except Label.DoesNotExist:
+            logger.info(f"{label_name} label does not exist")
             return Response({'response_msg': f'{label_name} label is not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
@@ -112,17 +167,30 @@ class DeleteLabelAPI(GenericAPIView):
 class AddAndRetrieveNotesForSpecificLabelAPI(GenericAPIView):
     serializer_class = AddNotesForSpecificLabelSerializer
     def get(self, request, label_name):
+        """
+        This API is used to get label specific notes
+        @param label_name: existing specific label name
+        @return: Retrieves note for this specific label
+        """
         try:
             label = Label.objects.get(Q(label_name=label_name) & Q(user_id=request.user.pk))
             notes = Notes.objects.filter(Q(label=label) & Q(user=request.user.pk) & Q(is_archive=False))
             if not notes:
+                logger.info(f"No notes available for label '{label_name}' ")
                 return Response({'response_data': f"No notes available for label '{label_name}' "}, status=status.HTTP_200_OK)
             serializer = RetriveAllNotesSerializer(notes, many=True)
+            logger.info(f"all notes of {label_name} are fetched")
             return Response({'response_data':serializer.data}, status=status.HTTP_200_OK)
         except (Label.DoesNotExist, Notes.DoesNotExist):
+            logger.info(f'{label_name} label is not exist')
             return Response({'response_msg': f'{label_name} label is not exist'}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request, label_name):
+        """
+        This API is used to create label specific notes
+        @param label_name: existing specific label name
+        @return: creates note for this specific label
+        """
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             try:
@@ -131,29 +199,46 @@ class AddAndRetrieveNotesForSpecificLabelAPI(GenericAPIView):
                              content=serializer.data.get('content'),color=serializer.data.get('color'))
                 note.save()
                 note.label.add(label)
+                logger.info(f"Note is saved")
                 return Response({'response_msg':'Your note is saved'}, status=status.HTTP_201_CREATED)
             except Label.DoesNotExist:
+                logger.info(f'{label_name} label is not exist')
                 return Response({'response_msg': f'{label_name} label is not exist'}, status=status.HTTP_404_NOT_FOUND)
+        logger.error(serializer.errors)
         return Response({'response_msg': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class TrashNotesAPI(GenericAPIView):
     serializer_class = RetriveAllNotesSerializer
     def get(self, request):
+        """
+        This API is used to get all trashed notes
+        @param request: get request
+        @return: Returns all trashed notes
+        """
         notes =Notes.objects.filter(Q(user=request.user.pk) & Q(is_trash=True))
         if not notes:
+            logger.info(f"Trash is empty")
             return Response({'response_data': 'No notes in Trash'}, status=status.HTTP_404_NOT_FOUND)
         serializer = self.serializer_class(notes, many=True)
+        logger.info(f"All trashed notes are accessed")
         return Response({'response_data':serializer.data}, status=status.HTTP_200_OK)
 
 @method_decorator(login_required(login_url='/user/login/'), name='dispatch')
 class UnTrashNotesAPI(GenericAPIView):
     def patch(self, request, note_id):
+        """
+        This API is used to restore trashed note
+        @param note_id: primary_key of the specific note
+        @return: Restores the note
+        """
         try:
             note = Notes.objects.get(Q(pk=note_id) & Q(user=request.user.pk) & Q(is_trash=True))
             note.is_trash = False
             note.save()
+            logger.info("Note is restored")
             return Response({'response_msg': 'Your Note is restored'}, status=status.HTTP_200_OK)
         except Notes.DoesNotExist:
+            logger.info("Note does not exist")
             return Response({'response_msg': 'This Note is not exist'}, status=status.HTTP_404_NOT_FOUND)
 
